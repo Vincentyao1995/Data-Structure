51506a990de05eb2ca4778dd5caaeb9a93768635
Vincent_Yao <vincent_yao1995@foxmail.com>
2017-06-28 18:32:07 -0700

    Exp3 extracts SP paras, classifier is next step. Find_strange is to deal with acc_res.txt and got acc_filtered

--- Sur&Oxi_Classify/SAM/find_strange_points.py
+++ Sur&Oxi_Classify/SAM/find_strange_points.py
@@ -20,31 +20,31 @@
         name, single_acc, all_acc = line.split('\t')
 
         if (float(single_acc) <= 0.90 and  float(single_acc) >= 0.80) and (float(all_acc) <= 0.90 and  float(all_acc) >= 0.80):
-            file_acc_filter.write("%s \t %s \t %s\n" % (name, single_acc, all_acc))
+            file_acc_filter.write("%s \t %s \t %s" % (name, single_acc, all_acc))
             count_medium += 1
 
     #count high acc
-    file_acc_filter.write('low acc:\n')
+    file_acc_filter.write('\nlow acc:\n')
     for line in lines[1:-1]:
         
         name, single_acc, all_acc = line.split('\t')
 
         if float(single_acc) < 0.80 and  float(all_acc) < 0.80:
-            file_acc_filter.write("%s \t %s \t %s\n" % (name, single_acc, all_acc))
+            file_acc_filter.write("%s \t %s \t %s" % (name, single_acc, all_acc))
             count_low += 1
 
-    file_acc_filter.write('high acc:\n')
+    file_acc_filter.write('\nhigh acc:\n')
     for line in lines[1:-1]:
         
         name, single_acc, all_acc = line.split('\t')
 
         if float(single_acc) >= 0.90 or  float(all_acc) > 0.90:
-            file_acc_filter.write("%s \t %s \t %s\n" % (name, single_acc, all_acc))
+            file_acc_filter.write("%s \t %s \t %s" % (name, single_acc, all_acc))
             count_high += 1
 
 
     allNum = len(lines) - 1
-    file_acc_filter.write("Total acc  \t  >0.9  %f  \t <0.8  %f \t 0.8-0.9  %f \n" % ((allNum - count_low - count_medium)/allNum ,count_low / allNum, count_medium / allNum))
+    file_acc_filter.write("\nTotal acc  \t  >0.9  %f  \t <0.8  %f \t 0.8-0.9  %f \n" % ((allNum - count_low - count_medium)/allNum ,count_low / allNum, count_medium / allNum))
     #print("Total acc  \t  >0.9  %f  \t <0.8  %f \t 0.8-0.9  %f \n" % ((allNum - count_low - count_medium)/allNum ,count_low / allNum, count_medium / allNum))
     file_acc_filter.close()
     print('done')
